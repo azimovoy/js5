8d34cf3ba26cdfe0697f2b65f2a15f0e28d6cc68
oybek <123@.uz>
2023-09-28 15:17:08 +0500

    safe

---
+++ app.js
@@ -0,0 +1,32 @@
+//for (i = 1; i <= 10; i++ ){
+//let imya = +prompt('Ввелите имя пользователя');
+///let age = +prompt('Введите возраст пользователя');
+
+//const user = {
+// userName: imya,
+//userAge: age
+//}
+//console.log('Пользователь ' + i);
+//console.log('Имя пользователя: ' + imya);
+//console.log('Возраст пользователя: ' + age);
+
+//}
+
+
+
+//let rec = receipt();
+//let a = "доставка 9000";
+//let b = "Вы заказали ";
+
+
+//let sum = 0;
+//for (let key in rec) {
+  //sum = sum + rec[key].price;
+ // let x =  key;
+//let y = rec[key].info;
+//let z = rec[key].price;
+//let food = (x + y + z)
+
+//console.log(b + food + 'C вас ' + sum + a);
+
+//}
